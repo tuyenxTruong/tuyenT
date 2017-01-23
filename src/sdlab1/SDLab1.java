44008428ecf2119dba5f075fb7c42d3123c7a5d5
tue51138 <tue51138@cis-se35725.tu.temple.edu>
2017-01-23 14:14:22 -0500

    Hello World

---
+++ src/sdlab1/SDLab1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdlab1;
+
+/**
+ *
+ * @author tue51138
+ */
+public class SDLab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                System.out.println("Hello, World");
+
+    }
+    
+}
